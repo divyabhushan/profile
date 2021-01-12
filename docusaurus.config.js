0c047a155d649683c90e7ff02ea0cb2c1bd52d18
divyabhushan <divya_bhushan@hotmail.com>
2021-01-12 13:52:12 +0100

    Updating the Google Analytics Id

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: "divyabhushan", // Usually your GitHub org/user name.
   projectName: "my-portfolio", // Usually your repo name.
-  plugins: ['@docusaurus/plugin-google-analytics'],  
+  plugins: [['@docusaurus/plugin-google-analytics',{id:'my-portal'}]],  
   themeConfig: {
     googleAnalytics: {
       trackingID: 'UA-139623144-1',
